3f87c799d82b5b809044997cf99c5f825e3345a2
Alfan Nasrulloh <mail@alfannas.com>
2022-11-11 16:50:49 +0700

    tambah form insert

---
+++ insert.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert</title>
+</head>
+<body>
+    <h1>Tambah Data</h1>
+    <form action="action.php" method="post">
+        NIM: <input type="text" name="name"><br>
+        Nama: <input type="text" name="name"><br>
+        ID Jurusan: <input type="number" name="name"><br>
+        Jenis Kelamin: <input type="text" name="name"><br>
+        Tempat Lahir: <input type="text" name="name"><br>
+        Tanggal Lahir (yyyy-mm-dd): <input type="text" name="name"><br>
+        Alamat: <input type="text" name="name"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
